a3e2d39c098af6ca91d4b4869b928235e9802d68
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-09 14:02:18 +0200

    Winner gets declared

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let gameArray = [];
+let gameArray2player = [];
+
 const GamePlay = () => {
     let player1Symbol = 'X'
     let player2Symbol = 'O'
@@ -13,7 +16,49 @@ const GamePlay = () => {
 			return player2Symbol;
 	}
     };
-    return { ChangePlayer }
+
+	const GameLogic = (whichPlayer,array) =>{
+		let winCombos = [
+			[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7],
+		];
+		// Adds game id and which player to gameArray
+		for (let x = 1; x < 10; x++){
+			const gameBoardPlayerPosition = document.querySelector(`#id${x}p${whichPlayer}`);
+			if (gameBoardPlayerPosition == null){
+				continue
+			}
+			// So it doesnt add Duplicates in array 
+			else if (array.includes(gameBoardPlayerPosition.id)){
+				continue;
+			}
+			else{
+				array.push(gameBoardPlayerPosition.id)
+		}
+		}
+	}
+	// FindS Winner 
+	const FindWinner = (playerWhoWon,array) =>{
+		let winCombos = [
+			[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7],
+		];
+		let arrayToCompare = [];
+
+		// Pushes players markers ID to arrayToCompare
+		array.forEach((placedId) => {
+			const compareToWinCombos = placedId.slice(2,3)
+			arrayToCompare.push(Number(compareToWinCombos))
+	});
+		// Checks if anyone WON with arrayToCompare and WinCombos
+		winCombos.forEach((oneWinCombo) => {
+			if (oneWinCombo.every(val => arrayToCompare.includes(val))){
+				console.log(playerWhoWon)
+
+			}
+		});
+	}
+
+    // Plays the game AND displays it on Screen
+    return { ChangePlayer ,GameLogic ,FindWinner}
 }
 
 // Render GameBoard on Webpage
@@ -27,18 +72,28 @@ const GameBoardContents = () => {
                     const classList = square.classList;
                     const secondClass = classList[1];
                     const classNumber = secondClass.slice(-1);
+
                     // if ID already exists dont add new one
-                    if (square.id)
+                    if (square.id){
                         return;
+					}
                     else{
 						// Adds ID with same Number as class | Can track player position
                         square.setAttribute('id', `id${classNumber}p${whichPlayer}`);
 
 						// Change player turn 
-						const { ChangePlayer } = GamePlay();
+						const { ChangePlayer, GameLogic, FindWinner} = GamePlay();
                         square.textContent = ChangePlayer(whichPlayer)
-						whichPlayer === 1? whichPlayer = 2 : whichPlayer = 1
 
+						if (whichPlayer === 1){
+							GameLogic(whichPlayer,gameArray)
+							FindWinner(whichPlayer,gameArray)
+						}
+						else{
+							GameLogic(whichPlayer,gameArray2player)
+							FindWinner(whichPlayer,gameArray2player)
+						}
+						whichPlayer === 1? whichPlayer = 2 : whichPlayer = 1
                     }
             }
             });
